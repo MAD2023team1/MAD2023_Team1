ba599ffc9df3e4636138fe5560f228b41081374d
Geraldine Koh <geraldine.jx@gmail.com>
2023-06-14 20:12:56 +0800

    initial commit

---
+++ app/src/main/java/com/team1/book_my_campus/LoginPage.java
@@ -0,0 +1,46 @@
+package com.team1.book_my_campus;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity {
+
+    String title = "Login Page";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+        Log.v(title, "Create");
+
+        Intent myRecvIntent = getIntent();
+        EditText etEmail = findViewById(R.id.editTextText);
+        EditText etPassword = findViewById(R.id.editTextText2);
+        Button loginButtonToApp = findViewById(R.id.button);
+
+        loginButtonToApp.setOnClickListener(new View.OnClickListener() {
+            String myEmail;
+            String myPassword;
+
+            @Override
+            public void onClick(View v) {
+                Log.v(title,"Login button to app Pressed!");
+                myEmail = etEmail.getText().toString();
+                myPassword = etPassword.getText().toString();
+
+                Intent myIntent = new Intent(LoginPage.this,HomePage.class);
+                myIntent.putExtra("email",myEmail);
+                myIntent.putExtra("password", myPassword);
+                startActivity(myIntent);
+                Log.v(title,"Extrcted Email "+ myEmail + "Password "+ myPassword);
+            }
+        });
+    }
+}
--- app/src/main/java/com/team1/book_my_campus/MainActivity.java
+++ app/src/main/java/com/team1/book_my_campus/MainActivity.java
@@ -2,13 +2,70 @@ package com.team1.book_my_campus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    String title = "Main Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(title, "Starting App");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.v(title, "On Pause!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(title, "On Resume!");
+
+        Button loginButton = findViewById(R.id.button5);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(title, "Login button is pressed");
+                Intent myIntent = new Intent(MainActivity.this,LoginPage.class);
+                Log.v(title, "test");
+                startActivity(myIntent);
+                Log.v(title, "test2");
+
+            }
+        });
+
+        Button signUpButton = findViewById(R.id.button3);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(title, "Sign Up button is pressed");
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(title, "On Stop!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(title, "On Destroy!");
     }
 }
