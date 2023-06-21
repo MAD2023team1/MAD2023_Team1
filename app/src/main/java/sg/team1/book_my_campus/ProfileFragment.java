75ca9f2123e027369dbcc3702cd05bae914c76ce
SQL <SQL@DESKTOP-OUVCINF>
2023-06-21 21:12:53 +0800

    changes to button and text color for edit and viewprofile

--- app/src/main/java/sg/team1/book_my_campus/ProfileFragment.java
+++ app/src/main/java/sg/team1/book_my_campus/ProfileFragment.java
@@ -76,6 +76,7 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 HomePage ParentActivity = (HomePage) getActivity();
+                //
                 // ParentActivity.switchToEditProfile(EditProfileFragment.newInstance(mParam1, mParam2));
             }
         });
