9193a9db9b5e77ba6e9584a52ff2ce70f32cd42d
tan-li-yan <liyaentan@gmail.com>
2023-07-17 17:46:55 +0800

    fixed the upper navigation bar's animation for consistency

--- app/src/main/java/sg/team1/book_my_campus/favouritesFragment.java
+++ app/src/main/java/sg/team1/book_my_campus/favouritesFragment.java
@@ -1,10 +1,9 @@
 package sg.team1.book_my_campus;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
 
-import androidx.appcompat.widget.AppCompatToggleButton;
+
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,7 +23,7 @@ import java.util.Set;
  * create an instance of this fragment.
  */
 public class favouritesFragment extends Fragment implements RecyclerViewInterface{
-    ArrayList<String> roomNameFavourites = new ArrayList<>();
+    ArrayList<Room> roomFavourites = new ArrayList<>();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -43,26 +42,27 @@ public class favouritesFragment extends Fragment implements RecyclerViewInterfac
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+
      * @return A new instance of fragment favouritesFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static favouritesFragment newInstance(String param1, String param2) {
+    public static favouritesFragment newInstance(ArrayList<Room> favoriteRoomsList) {
         favouritesFragment fragment = new favouritesFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putParcelableArrayList("favRoomList", favoriteRoomsList);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            roomFavourites = getArguments().getParcelableArrayList("favRoomList");
+        }
+        else{
+            Log.d("Favourite Fragment", "Room List is empty");
         }
     }
 
@@ -73,28 +73,12 @@ public class favouritesFragment extends Fragment implements RecyclerViewInterfac
 
         View rootView = inflater.inflate(R.layout.fragment_favourites, container, false);
         // Retrieve room information and liked status from the intent
-        // Retrieve room information and liked status from shared preferences
-        SharedPreferences preferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        String roomName = preferences.getString("roomName", "");
-        boolean isLiked = preferences.getBoolean("isRoomLiked", true);
-        Log.d("FavouritesFragment", "roomName: " + roomName.toString());
-        Log.d("FavouritesFragment", "isLiked: " + String.valueOf(isLiked));
-        // Update the liked rooms based on the status
-        if (isLiked == true) {
-            roomNameFavourites.add(roomName);
-            Log.d("FavouritesFragment", "Room Favourites: " + roomNameFavourites.toString());
+            RecyclerView recyclerView = rootView.findViewById(R.id.favRecyclerView);
+            //Create adapter after setting up models
+            favourites_adapter adapter = new favourites_adapter(getContext(),roomFavourites,this);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
 
-        } else {
-            roomNameFavourites.remove(roomName);
-            Log.d("FavouritesFragment", "Room Favourites: " + roomNameFavourites.toString());
-        }
-
-        //Toggle button
-        RecyclerView recyclerView = rootView.findViewById(R.id.favRecyclerView);
-        //Create adapter after setting up models
-        favourites_adapter adapter = new favourites_adapter(getContext(),roomNameFavourites,this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
         return rootView;
 
     }
