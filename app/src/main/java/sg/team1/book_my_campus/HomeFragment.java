5adb35b30b4b8377c1e07f374a080be3a5656e34
tan-li-yan <liyaentan@gmail.com>
2023-07-28 17:18:50 +0800

    Top 3 rooms (need to fix the code later)

--- app/src/main/java/sg/team1/book_my_campus/HomeFragment.java
+++ app/src/main/java/sg/team1/book_my_campus/HomeFragment.java
@@ -10,6 +10,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Hashtable;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -25,8 +29,15 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Text;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -37,6 +48,7 @@ public class HomeFragment extends Fragment {
     String title = "HomeFragment";
     ArrayList<Ratings> ratingsRoomArrayList = new ArrayList<>();
     ArrayList<Ratings> topRatedRoomList = new ArrayList<>();
+    ArrayList<top_rated_room_model> top_rated_room_modelsList = new ArrayList<>();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -78,6 +90,7 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -97,6 +110,9 @@ public class HomeFragment extends Fragment {
             // If the user does not have a profile picture, set the default image resource
             homeProfile.setImageResource(R.drawable.baseline_person_24);
         }
+
+        readRatingsDocument();
+
         // Inflate the layout for this fragment
         return inflatedHomeView;
 
@@ -120,15 +136,117 @@ public class HomeFragment extends Fragment {
                             Log.v(title,"List size"+ratingsRoomArrayList.size());
 
                         }
+                        HashMap<String, Float> sumRatings = new HashMap<String, Float>();
+                        HashMap<String, Integer> countRatings = new HashMap<String,Integer>();
 
-                        for(Ratings room: ratingsRoomArrayList){
-                            //Find unique roomnames
-                            //Match the roomnames with each room.getName
-                            //add dictionary and divide by the number of roomsn, update dictionary accordingly.
-                            //limit the array to only 5 rooms and sort them from largest to smallest
-                            //then add them to an adapter? to show top 5 rating rooms
+                        for(Ratings rating:ratingsRoomArrayList){
+                            String roomName = rating.getRoomName();
+                            sumRatings.put(roomName,0.0f);
+                            countRatings.put(roomName,0);
                         }
+
+                        for(Ratings rating: ratingsRoomArrayList){
+                            String roomName = rating.getRoomName();
+                            float starRating = rating.getStarRatings();
+                            Log.v(title, "Room Name:" + roomName);
+                            Log.v(title, "Star Rating" +starRating);
+                            if(sumRatings.containsKey(roomName)){
+                                Log.v(title, "While Loop: Before Count Ratings Hash Map Mapping:" + countRatings);
+                                float previousSumValue = sumRatings.get(roomName);
+                                int previousCount = countRatings.get(roomName);
+                                Log.v(title, "Before Addition:" + previousCount);
+                                previousCount += 1;
+                                previousSumValue += starRating;
+                                Log.v(title, "After addition:" +previousCount);
+                                sumRatings.put(roomName,previousSumValue);
+                                countRatings.put(roomName,previousCount);
+                                Log.v(title, "While Loop: After Count Ratings Hash Map Mapping:" + countRatings);
+                            }
+                        }
+                        Log.v(title, "Sum Ratings Hash Map Mapping:" + sumRatings);
+                        Log.v(title, "Count Ratings Hash Map Mapping:" + countRatings);
+                        //calculate the average by matching they keys together and then dividing the values
+                        HashMap<String, Float> averageRatings = new HashMap<>();
+                        LinkedHashMap<String,Float> sortedAverage = new LinkedHashMap<>();
+                        ArrayList<Float> list = new ArrayList<>();
+                        for (String room : sumRatings.keySet()) {
+                            if (countRatings.containsKey(room)) {
+                                float sum = sumRatings.get(room);
+                                int count = countRatings.get(room);
+                                float average = sum / count;
+                                averageRatings.put(room, average);
+                            }
+                        }
+                        Log.v(title, "Average Ratings Hash Map Mapping:" + averageRatings);
+                        //Sort the average list from largest to smallest
+                        for (Map.Entry<String, Float> entry : averageRatings.entrySet()) {
+                            list.add(entry.getValue());
+                        }
+                        Collections.sort(list);
+                        for (float num : list) {
+                            for (Map.Entry<String, Float> entry : averageRatings.entrySet()) {
+                                if (entry.getValue().equals(num)) {
+                                    sortedAverage.put(entry.getKey(), num);
+                                }
+                            }
+                        }
+                        Log.v(title, "Average Ratings Hash Map MSorted:" + sortedAverage);
+                        //convert
+                        List<Map.Entry<String,Float>> entryAvg = new ArrayList<>(sortedAverage.entrySet());
+                        Log.v(title, "This is the list of sorted:" + entryAvg.toString());
+                        Log.v(title, "This is the first index:" + entryAvg.get(0).getValue());
+                        //Extract the top 3 values(the last entry is the highest value)
+                        int lastIndex = sortedAverage.size()-1;
+
+
+
+                        //obtaining room Names from the room dataset stored in strings.xml
+                        String[] roomNamesFromString = getResources().getStringArray(R.array.room_name_full_text);
+                        int[] roomImages = {R.drawable.ispace, R.drawable.smartcube1and2, R.drawable.smartcube1and2, R.drawable.smartcube3and4, R.drawable.smartcube3and4};
+                        // loop through this if both name matches with the top 3, create a top3 room model.
+                        for(String name: roomNamesFromString){
+                            //get the name of the top 3 rated room Names
+                            if (entryAvg.size() !=0){
+                                RecyclerView recyclerView = inflatedHomeView.findViewById(R.id.topRoomRecyclerView);
+                                String topRatedRoom1 = entryAvg.get(lastIndex).getKey();
+                                if(name.equals(topRatedRoom1)){
+                                    //convert this array into a list to find the index of the roomName as all of the image are in the same order as well
+                                    //array does not have the method index Of
+                                    float topRatedRoom1Ratings = entryAvg.get(lastIndex).getValue();
+                                    int indexTopRatedRoom = Arrays.asList(roomNamesFromString).indexOf(topRatedRoom1);
+                                    top_rated_room_model roomRating = new top_rated_room_model(topRatedRoom1Ratings, roomImages[indexTopRatedRoom],topRatedRoom1);
+                                    top_rated_room_modelsList.add(roomRating);
+                                    Log.v(title, "Name in our database:" + name);
+                                    top_room_adapter adapter = new top_room_adapter(getContext(), top_rated_room_modelsList);
+                                    recyclerView.setAdapter(adapter);
+                                    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                                }
+                                if(entryAvg.size() ==2){
+                                    String topRatedRoom2 = entryAvg.get(lastIndex-1).getKey();
+                                    if(name.equals(topRatedRoom1) || name.equals(topRatedRoom2)){
+                                        Log.v(title, "Name in our database:" + name);
+                                    }
+                                }
+                                if(entryAvg.size() == 3){
+                                    String topRatedRoom2 = entryAvg.get(lastIndex-1).getKey();
+                                    String topRatedRoom3 = entryAvg.get(lastIndex-2).getKey();
+                                    if(name.equals(topRatedRoom1) || name.equals(topRatedRoom2) || name.equals(topRatedRoom3)){
+                                        Log.v(title, "Name in our database:" + name);
+                                    }
+                                }
+
+
+                                //check if the name in the room equals to the 3 rooms above
+
+
+                            }
+
+
+                        }
+
                     }
+
+
 
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -137,5 +255,9 @@ public class HomeFragment extends Fragment {
                         Log.v(title, "onFailure: ", e);
                     }
                 });
+    }
+
+    private void setUpRateRoomModel(){
+
     }
 }
